5d7bb1ddc805004d4005ed5c0bb19128abd1af29
Joanna Edgell <joedgell2013@gmail.com>
2014-10-29 17:24:13 -0400

    i hate you

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,15 +1,14 @@
-var my_server= 'http://tiy-atl-fe-server.herokuapp.com/collections/chelseaandjoanna';
+var my_server= 'http://tiy-atl-fe-server.herokuapp.com/collections/chelseaandjoanna2';
 
 var ToDo= function (options) {
 	options= options||{};
 	this.done= 'false';
-	this.task=options.task || '';
-	this.count = 1;
+	this.task= options.task || '';
 
 };
 
+var all=[];
 var todo_list;
-
 var task_template= $('#task_items').html();
 var rendered = _.template(task_template);
 
@@ -18,36 +17,55 @@ $.getJSON(my_server).done( function(data){
 	todo_list = data;
 
 	_.each(todo_list, function(item){
-		$('todoitems').append(rendered(item));
+
+		$('.toDoItems').append(rendered(item));
 	});
 });
 
 var task, contents;
-
 $('#button').on('click', function(event){
-event.preventDefault();
-var self=this;
-contents = $('.input').val();
+	event.preventDefault();
 
-task= new ToDo({
-	task: contents
+
+	var self=this;
+	contents = $('.input').val()+ '<button id= "away">x</button>';
+
+	task= new ToDo({
+		task: contents
+		});
+	console.log(task);
+
+	$.ajax({
+		type: 'POST',
+		url: my_server,
+		data: task
+	}).done( function(data){
+
+			todo_list.push(data);
+
+			$('.toDoItems').append(rendered(data));
+
+		  $('.input').val('');
+
+			var q =$('.toDoItems li').length;
+				$('.total').html(q);
+
+		});
+
 });
-console.log(task);
 
-$.ajax({
-	type: 'POST',
-	url: my_server,
-	data: task
-}).done( function(data){
 
-	todo_list.push(data);
+$('ul').on('click', '#away', function(){
+	$(this).parent().remove();
 
-	$('.toDoItems').append(rendered(data));
+	var q = $('.toDoItems li').length - 0;
+		$('.total').html(q);
 
-	$('.input').val('');
+		$.ajax({
+			type: 'DELETE',
+			url: my_server + "/" + todo_modifier._id,
+		});
 });
-});
-
 
 
 var todo_modifier;
@@ -61,7 +79,11 @@ todo_modifier=_.findWhere(todo_list, {_id: myID});
 	if(todo_modifier.done ==='true'){
 		todo_modifier.done = 'false';
 		$(this).removeClass('completed');
-	}else{
+
+		var w=$('.toDoList .done').length-0;
+			$('.finished').html(w);
+	}
+	else{
 		todo_modifier.done ='true';
 
 		$(this).addClass('completed');
@@ -72,7 +94,8 @@ todo_modifier=_.findWhere(todo_list, {_id: myID});
 $.ajax({
 	type: 'PUT',
 	url: my_server + "/" + todo_modifier._id,
-	data:todo_modifier
+	data: todo_modifier,
 });
+
 
 });
